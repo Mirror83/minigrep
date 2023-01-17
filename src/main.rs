09bc58aa96845a73aa31d369ac94e1452d271f22
unknown <glenochieng045@gmail.com>
2023-01-17 20:53:39 +0300

    Refactored to use the configuraction struct Config

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,36 @@
 use std::env;
 use std::fs;
 
+#[derive(Debug)]
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Self {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Config {
+            query, file_path
+        }
+    }
+}
+
 fn main() {
     // The collect function turns an iterator into a collection of the annotated type
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = Config::new(&args);
+    println!("{:?}", config);
 
-    println!("Searching for '{query}'");
-    println!("In '{file_path}'");
+    println!("Searching for '{}'", config.query);
+    println!("In '{}'", config.file_path);
 
-    let content = fs::read_to_string(file_path).expect("Could not read the file");
+    let content = fs::read_to_string(config.file_path)
+        .expect("Could not read the file");
 
-    println!("With content:\n{content}");
+    println!("With content:\n{}", content);
 
 }
