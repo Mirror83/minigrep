02fb7bba81419e9e444e64dc4899bd236a5b7c14
unknown <glenochieng045@gmail.com>
2023-01-17 21:26:01 +0300

    Changed the new function into the build function

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{env, process};
 use std::fs;
 
 #[derive(Debug)]
@@ -8,13 +8,19 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Self {
+    // As programmers do not expect new to fail, we change the name of the
+    // function from new to build
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments were provided")
+        } else if args.len() > 3 {
+           return Err("Too many arguments were provided")
+        }
+
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Config {
-            query, file_path
-        }
+        Ok(Config { query, file_path })
     }
 }
 
@@ -22,14 +28,21 @@ fn main() {
     // The collect function turns an iterator into a collection of the annotated type
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
-    println!("{:?}", config);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        // A non-zero exit code signifies erroneous termination
+        process::exit(1);
+    });
+
+    let content = fs::read_to_string(&config.file_path)
+        .unwrap_or_else(|err| {
+            println!("Problem opening the file: {}", err);
+            // A non-zero exit code signifies erroneous termination
+            process::exit(1);
+        });
 
     println!("Searching for '{}'", config.query);
     println!("In '{}'", config.file_path);
-
-    let content = fs::read_to_string(config.file_path)
-        .expect("Could not read the file");
 
     println!("With content:\n{}", content);
 
